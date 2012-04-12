33b267cab3118bec73b8c88c3837d75a2a2ee28b
jgeboski <jgeboski@gmail.com>
2012-04-12 06:41:57 -0400

    monitorix: upgraded to 2.5.0

--- monitorix/doinst.sh
+++ monitorix/doinst.sh
@@ -8,4 +8,16 @@ config() {
   fi
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 config etc/monitorix.conf.new
+preserve_perms etc/rc.d/rc.monitorix.new
