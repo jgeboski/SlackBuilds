1759723109d61d439ed8c6a285216a172c0720bc
jgeboski <jgeboski@gmail.com>
2012-10-05 16:10:34 -0400

    openjdk: moved profile.d scripts inside SlackBuild

--- development/openjdk/profile.d/openjdk.csh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-setenv JAVA_HOME @JAVA_HOME@
-setenv PATH ${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
-setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:@JAVA_LD@
--- development/openjdk/profile.d/openjdk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=@JAVA_HOME@
-export PATH="${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:@JAVA_LD@"
