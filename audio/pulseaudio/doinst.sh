fa29f5fe78729822534e2444543ced40f720aee8
jgeboski <jgeboski@gmail.com>
2012-11-21 15:30:25 -0500

    pulseaudio: added

---
+++ audio/pulseaudio/doinst.sh
@@ -0,0 +1,27 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/pulse/client.conf.new
+config etc/pulse/daemon.conf.new
+config etc/pulse/default.pa.new
+config etc/pulse/system.pa.new
+
+preserve_perms etc/rc.d/rc.pulseaudio.new
